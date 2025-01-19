6abfe5dbb7525ed80bb88abde4626719e85eb7a1
Anertz <84836743+Anertz@users.noreply.github.com>
2025-01-20 03:37:31 +0900

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,4 @@
-git clone https://github.com/Anertz/AFeMS; cd AFeMS; (pwd).Path | clip; echo "ModsのインストールとPathのコピーが完了しました"
+git clone https://github.com/Anertz/AFeMS
+cd AFeMS
+(pwd).Path | clip
+echo "ModsのインストールとPathのコピーが完了しました"
