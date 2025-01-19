afb8197e533773c6eae7573f4625705419ec8aa7
Anertz <84836743+Anertz@users.noreply.github.com>
2025-01-20 04:07:52 +0900

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -2,7 +2,7 @@ git clone https://github.com/Anertz/AFeMS
 if (Test-Path "AFeMS") {
     cd AFeMS
     (pwd).Path | clip
-    Write-Host "Nice! Mods are installed" -ForegroundColor Green
+    Write-Host "Nice! Mods are installed" -ForegroundColor DarkCyan
 } else {
     Write-Host "Git clone was failed" -ForegroundColor Red
 }
