98a321bc48eec14ec0c22391df2af2552304649c
Anertz <84836743+Anertz@users.noreply.github.com>
2025-01-20 03:53:10 +0900

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ git clone https://github.com/Anertz/AFeMS
 if (Test-Path "AFeMS") {
     cd AFeMS
     (pwd).Path | clip
-    echo "ModsのインストールとPathのコピーが完了しました"
+    Write-Host "Mods are installed!" -ForegroundColor Green
 } else {
-    Write-Host "Git clone に失敗しました。" -ForegroundColor Red
+    Write-Host "Git clone was failed" -ForegroundColor Red
 }
